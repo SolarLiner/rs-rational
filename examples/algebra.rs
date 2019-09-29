85ebfb6ae2eac8b46b9679db7dbc2fe2200ff48d
Nathan Graule <solarliner@gmail.com>
2019-09-29 12:02:30 +0200

    Add matrix-vector example

--- examples/algebra.rs
+++ examples/algebra.rs
@@ -1,4 +1,4 @@
-use nalgebra::Vector3;
+use nalgebra::{Vector3, Vector2, Matrix2};
 use rational::Rational;
 
 fn main() {
@@ -6,4 +6,9 @@ fn main() {
     let q = Vector3::new(Rational::new(3, 5), Rational::new(1, 2), Rational::from(0));
 
     println!("{} + {} = {}", mat, q, mat+q);
+
+    let mat = Matrix2::new(Rational::from(0), Rational::from(1), Rational::new(1, 2), Rational::new(3, 4));
+    let vec = Vector2::new(Rational::new(1, 3), Rational::new(1, 2));
+
+    println!("{} * {} = {}", mat, vec, mat*vec);
 }
